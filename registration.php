53bf86b6ec1fbf1c944c68f09e54bce0b2ec72aa
Adele Clark <adeleclark@rezolve.com>
2019-09-29 14:23:00 +0300

    init module commit

---
+++ registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Adele_OrderExport', __DIR__);
